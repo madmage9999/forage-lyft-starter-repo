1066d9e4711e2cd2bb6dc68cc91971b65251da94
Steve <nornorfalcon@gmail.com>
2023-10-11 22:57:34 +1100

    progress on creating classes

--- car.py
+++ car.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+from engine import engine
 
 class Car(ABC):
     def __init__(self, last_service_date):
@@ -8,3 +8,11 @@ class Car(ABC):
     @abstractmethod
     def needs_service(self):
         pass
+
+class Calliope(Car):
+    self.engine = 
+
+class CarFactory():
+    def create_car(self, model):
+        target_class = model
+        return globals()[target_class]()
